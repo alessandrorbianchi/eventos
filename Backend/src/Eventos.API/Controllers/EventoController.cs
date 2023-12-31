009527ed51e5e88fd8ee25a1bb70310b4e762b93
Alessandro Bianchi <alessandro.r.bianchi@gmail.com>
2023-12-31 10:57:38 -0300

    criando evento model

--- Backend/src/Eventos.API/Controllers/EventoController.cs
+++ Backend/src/Eventos.API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Eventos.API.Models;
 
 namespace Eventos.API.Controllers;
 
@@ -6,14 +7,187 @@ namespace Eventos.API.Controllers;
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
 {
+    public IEnumerable<Evento> _evento = new Evento[] {
+            new() {
+                EventoId = 1,
+                Tema = "Angular 12 e .NET6",
+                Local = "BH",
+                Lote = "1° Lote",
+                QtdPessoas = 250,
+                DataEvento =  DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.jpg"
+            },
+            new () {
+                EventoId = 2,
+                Tema = "Angular 12 e .NET6",
+                Local = "BH",
+                Lote = "1° Lote",
+                QtdPessoas = 250,
+                DataEvento =  DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.jpg"
+            }
+        };
+
     public EventoController()
     {
     
     }
 
     [HttpGet]
-    public string Get()
+    public IEnumerable<Evento> Get()
     {
-        return "value";
+        return _evento;
+    }
+
+    [HttpGet("{id}")]
+    public IEnumerable<Evento> GetById(int id)
+    {
+        return _evento.Where(evento => evento.EventoId == id);
     }
 }
+
+
+/*
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly IEventoService _eventoService;
+        private readonly IUtil _util;
+        private readonly IAccountService _accountService;
+
+        private readonly string _destino = "Images";
+
+        public EventosController(IEventoService eventoService,
+                                 IUtil util,
+                                 IAccountService accountService)
+        {
+            _util = util;
+            _accountService = accountService;
+            _eventoService = eventoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] PageParams pageParams)
+        {
+            try
+            {
+                var eventos = await _eventoService.GetAllEventosAsync(User.GetUserId(), pageParams, true);
+                if (eventos == null) return NoContent();
+
+                Response.AddPagination(eventos.CurrentPage, eventos.PageSize, eventos.TotalCount, eventos.TotalPages);
+
+                return Ok(eventos);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(User.GetUserId(), id, true);
+                if (evento == null) return NoContent();
+
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPost("upload-image/{eventoId}")]
+        public async Task<IActionResult> UploadImage(int eventoId)
+        {
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(User.GetUserId(), eventoId, true);
+                if (evento == null) return NoContent();
+
+                var file = Request.Form.Files[0];
+                if (file.Length > 0)
+                {
+                    _util.DeleteImage(evento.ImagemURL, _destino);
+                    evento.ImagemURL = await _util.SaveImage(file, _destino);
+                }
+                var EventoRetorno = await _eventoService.UpdateEvento(User.GetUserId(), eventoId, evento);
+
+                return Ok(EventoRetorno);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar realizar upload de foto do evento. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(EventoDto model)
+        {
+            try
+            {
+                var evento = await _eventoService.AddEventos(User.GetUserId(), model);
+                if (evento == null) return NoContent();
+
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar adicionar eventos. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, EventoDto model)
+        {
+            try
+            {
+                var evento = await _eventoService.UpdateEvento(User.GetUserId(), id, model);
+                if (evento == null) return NoContent();
+
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar atualizar eventos. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(User.GetUserId(), id, true);
+                if (evento == null) return NoContent();
+
+                if (await _eventoService.DeleteEvento(User.GetUserId(), id))
+                {
+                    _util.DeleteImage(evento.ImagemURL, _destino);
+                    return Ok(new { message = "Deletado" });
+                }
+                else
+                {
+                    throw new Exception("Ocorreu um problem não específico ao tentar deletar Evento.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar deletar eventos. Erro: {ex.Message}");
+            }
+        }
+    }
+}
+
+*/
